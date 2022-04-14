bfab80707d706d8d5cb69d009e6a8625b1ec0dea
Filipe Souza Almeida <filipe.almeida@mov.ai>
2022-04-14 14:06:12 +0100

    change system to gui to render visualization

--- material_controller/MaterialController.cc
+++ material_controller/MaterialController.cc
@@ -1,183 +1,242 @@
-#include "MaterialController.hh"
-#include <string>
-#include <vector>
-
-#include <ignition/common/Profiler.hh>
 #include <ignition/plugin/Register.hh>
-#include <ignition/transport/Node.hh>
-
-#include "ignition/gazebo/components/Material.hh"
-#include "ignition/gazebo/components/Visual.hh"
-#include "ignition/gazebo/components/Name.hh"
-#include "ignition/gazebo/components/Geometry.hh"
-#include "ignition/gazebo/components/Transparency.hh"
-#include "ignition/common/Material.hh"
-#include "ignition/gazebo/Model.hh"
-#include "ignition/math/Color.hh"
-#include "ignition/gazebo/Util.hh"
-
-#include <ignition/gazebo/components/Geometry.hh>
-#include <ignition/gazebo/components/Light.hh>
-#include <ignition/gazebo/components/Link.hh>
-#include <ignition/gazebo/components/Material.hh>
-#include <ignition/gazebo/components/Model.hh>
 #include <ignition/gazebo/components/Name.hh>
+#include <ignition/gazebo/components/Model.hh>
+#include <ignition/gazebo/components/ChildLinkName.hh>
 #include <ignition/gazebo/components/ParentEntity.hh>
-#include <ignition/gazebo/components/Pose.hh>
-#include <ignition/gazebo/components/Transparency.hh>
-#include <ignition/gazebo/components/Visual.hh>
 #include <ignition/gazebo/components/World.hh>
 #include <ignition/gazebo/EntityComponentManager.hh>
-#include <ignition/gazebo/Util.hh>
+#include <ignition/gazebo/gui/GuiEvents.hh>
+#include "ignition/gazebo/components/Factory.hh"
 
-#include <sdf/Light.hh>
-#include <sdf/Mesh.hh>
-#include <sdf/Model.hh>
-#include <sdf/Visual.hh>
+#include <ignition/gui/Application.hh>
+//! [includeGuiEvents]
+#include <ignition/gui/GuiEvents.hh>
+//! [includeGuiEvents]
+#include <ignition/gui/MainWindow.hh>
+#include <ignition/math/Rand.hh>
+#include <ignition/plugin/Register.hh>
+#include <ignition/rendering/RenderEngine.hh>
+#include <ignition/rendering/RenderingIface.hh>
+#include <ignition/rendering/Scene.hh>
+#include <ignition/rendering/Visual.hh>
 
-#include <ignition/common/Material.hh>
-#include <ignition/common/MeshManager.hh>
-#include <ignition/common/Mesh.hh>
-#include <ignition/common/SubMesh.hh>
+#include "MaterialController.hh"
 
-#include <ignition/math/Matrix4.hh>
+/////////////////////////////////////////////////
+MaterialController::MaterialController() = default;
 
-using namespace ignition;
-using namespace gazebo;
-using namespace systems;
+/////////////////////////////////////////////////
+MaterialController::~MaterialController() = default;
 
-class ignition::gazebo::systems::MaterialControllerPrivate
+/////////////////////////////////////////////////
+void MaterialController::LoadConfig(const tinyxml2::XMLElement * /*_pluginElem*/)
 {
-  /// \brief Callback for joint force subscription
-  /// \param[in] _msg Joint force message
-  public: void OnChangeCollor(const ignition::msgs::Color &_msg);
-
-  /// \brief Ignition communication node.
-  public: transport::Node node;
-
-  /// \brief Joint Entity
-  public: Entity linkEntity;
-
-  /// \brief Joint name
-  public: std::string linkName;
-
-  /// \brief Commanded joint force
-  public: double jointForceCmd;
-
-  /// \brief mutex to protect jointForceCmd
-  public: std::mutex jointForceCmdMutex;
-
-  /// \brief Model interface
-  public: Model model{kNullEntity};
-
-  /// \brief color to apply
-  public: ignition::math::Color newEmissive;
-};
-
-//////////////////////////////////////////////////
-MaterialController::MaterialController()
-  : dataPtr(std::make_unique<MaterialControllerPrivate>())
-{
-}
-
-//////////////////////////////////////////////////
-void MaterialController::Configure(const Entity &_entity,
-    const std::shared_ptr<const sdf::Element> &_sdf,
-    EntityComponentManager &_ecm,
-    EventManager &/*_eventMgr*/)
-{
-  this->dataPtr->model = Model(_entity);
-
-  if (!this->dataPtr->model.Valid(_ecm))
-  {
-    ignerr << "MaterialController plugin should be attached to a model entity. "
-           << "Failed to initialize." << std::endl;
-    return;
-  }
-
-  auto sdfClone = _sdf->Clone();
-
-  // Get params from SDF
-  auto sdfElem = sdfClone->GetElement("link_name");
-  if (sdfElem)
-  {
-    this->dataPtr->linkName = sdfElem->Get<std::string>();
-  }
-
-  if (this->dataPtr->linkName == "")
-  {
-    ignerr << "MaterialController found an empty linkName parameter. "
-           << "Failed to initialize.";
-    return;
-  }
-
-  ignerr << "Link name: " << this->dataPtr->linkName << std::endl;
-
+  if (this->title.empty())
+    this->title = "GUI system plugin";
+  ignition::gui::App()->findChild<
+      ignition::gui::MainWindow *>()->installEventFilter(this);
+  //! [connectToGuiEvent]
   // Subscribe to commands
-  auto topic = transport::TopicUtils::AsValidTopic("/model/" +
-      this->dataPtr->model.Name(_ecm) + "/link/" + this->dataPtr->linkName +
-      "/color");
+  auto topic = ignition::transport::TopicUtils::AsValidTopic("/model/color");
   if (topic.empty())
   {
-    ignerr << "Failed to create valid topic for [" << this->dataPtr->linkName
-           << "]" << std::endl;
+    ignerr << "Failed to create valid topic ]" << std::endl;
     return;
   }
-  this->dataPtr->node.Subscribe(topic, &MaterialControllerPrivate::OnChangeCollor,
-                                this->dataPtr.get());
+  this->node.Subscribe(topic, &MaterialController::OnChangeCollor,
+                                this);
 
   ignmsg << "MaterialController subscribing to Double messages on [" << topic
          << "]" << std::endl;
-  std::string topicName = "/model/" + this->dataPtr->model.Name(_ecm) + "/link/" + this->dataPtr->linkName + "/color";
+  std::string topicName = "/model/color";
   ignerr << "Subscribing to" << topicName << std::endl;
+  // Here you can read configuration from _pluginElem, if it's not null.
+}
+
+/////////////////////////////////////////////////
+void MaterialController::RandomColor()
+{
+  this->dirty = true;
 }
 
 //////////////////////////////////////////////////
-void MaterialController::PreUpdate(const ignition::gazebo::UpdateInfo &_info,
-    ignition::gazebo::EntityComponentManager &_ecm)
+void MaterialController::OnChangeCollor(const ignition::msgs::StringMsg &_msg)
 {
-  IGN_PROFILE("MaterialController::PreUpdate");
-
-  // If the joint hasn't been identified yet, look for it
-  if (this->dataPtr->linkEntity == kNullEntity)
+  std::vector<std::string> seglist = SplitMsg(_msg.data(), '-');
+  if (seglist.size() == 5)
   {
-    this->dataPtr->linkEntity =
-        this->dataPtr->model.LinkByName(_ecm, this->dataPtr->linkName);
+    this->linkName = seglist[0];
+    this->newEmissive.R() = std::stof(seglist[1]);
+    this->newEmissive.G() = std::stof(seglist[2]);
+    this->newEmissive.B() = std::stof(seglist[3]);
+    this->newEmissive.A() = std::stof(seglist[4]);
+    this->dirty = true;
+  }
+  else
+  {
+    ignerr << "No size match" << std::endl;
+  }
+}
+
+/////////////////////////////////////////////////
+//! [eventFilter]
+bool MaterialController::eventFilter(QObject *_obj, QEvent *_event)
+{
+  if (_event->type() == ignition::gui::events::Render::kType)
+  {
+    // This event is called in the render thread, so it's safe to make
+    // rendering calls here
+    this->PerformRenderingOperations();
   }
 
-  if (this->dataPtr->linkEntity == kNullEntity)
-    return;
+  // Standard event processing
+  return QObject::eventFilter(_obj, _event);
+}
+//! [eventFilter]
 
-  // Nothing left to do if paused.
-  if (_info.paused)
+/////////////////////////////////////////////////
+//! [performRenderingOperations]
+void MaterialController::PerformRenderingOperations()
+{
+  if (!this->dirty)
+  {
+    return;
+  }
+
+  if (nullptr == this->scene)
+  {
+    this->FindScene();
+  }
+
+  if (nullptr == this->scene)
     return;
   
-  auto visual = _ecm.EntityByComponents(
-      components::ParentEntity(this->dataPtr->linkEntity),
-      components::Visual());
+  auto visual = this->scene->VisualByName(this->linkName);
 
-  auto material = _ecm.Component<components::Material>(visual);
-  
-  material->Data().SetEmissive(this->dataPtr->newEmissive);
+  if (nullptr == visual)
+  {
+    ignerr << "No visual" << std::endl;
+  }
+
+  auto targetVis = std::dynamic_pointer_cast<ignition::rendering::Visual>(visual);
+
+  if (targetVis && targetVis->HasUserData("gazebo-entity"))
+  {
+    ignition::gazebo::Entity targetEntity =
+        std::get<int>(targetVis->UserData("gazebo-entity"));
+
+    ignition::rendering::MaterialPtr material = this->scene->CreateMaterial();
+    material->SetAmbient(this->newEmissive);
+    material->SetDiffuse(this->newEmissive);
+    // material->SetSpecular(1.0, 1.0, 1.0);
+    material->SetEmissive(this->newEmissive);
+    visual->SetMaterial(material);
+  }
+  else
+  {
+    ignerr << "Unable to find entity to change color" << std::endl;
+  }
+
+  this->dirty = false;
+}
+//! [performRenderingOperations]
+
+/////////////////////////////////////////////////
+void MaterialController::FindScene()
+{
+  auto loadedEngNames = ignition::rendering::loadedEngines();
+  if (loadedEngNames.empty())
+  {
+    igndbg << "No rendering engine is loaded yet" << std::endl;
+    return;
+  }
+
+  // assume there is only one engine loaded
+  auto engineName = loadedEngNames[0];
+  if (loadedEngNames.size() > 1)
+  {
+    igndbg << "More than one engine is available. "
+      << "Using engine [" << engineName << "]" << std::endl;
+  }
+  auto engine = ignition::rendering::engine(engineName);
+  if (!engine)
+  {
+    ignerr << "Internal error: failed to load engine [" << engineName
+      << "]. Grid plugin won't work." << std::endl;
+    return;
+  }
+
+  if (engine->SceneCount() == 0)
+  {
+    igndbg << "No scene has been created yet" << std::endl;
+    return;
+  }
+
+  // Get first scene
+  auto scenePtr = engine->SceneByIndex(0);
+  if (nullptr == scenePtr)
+  {
+    ignerr << "Internal error: scene is null." << std::endl;
+    return;
+  }
+
+  if (engine->SceneCount() > 1)
+  {
+    igndbg << "More than one scene is available. "
+      << "Using scene [" << scene->Name() << "]" << std::endl;
+  }
+
+  if (!scenePtr->IsInitialized() || nullptr == scenePtr->RootVisual())
+  {
+    return;
+  }
+
+  this->scene = scenePtr;
 }
 
 //////////////////////////////////////////////////
-void MaterialControllerPrivate::OnChangeCollor(const msgs::Color &_msg)
+void MaterialController::Update(const ignition::gazebo::UpdateInfo & _info,
+    ignition::gazebo::EntityComponentManager &_ecm)
 {
-  ignerr << "Received Color R: " << _msg.r() << std::endl;
-  ignerr << "Received Color G: " << _msg.g() << std::endl;
-  ignerr << "Received Color B: " << _msg.b() << std::endl;
-  ignerr << "Received Color A: " << _msg.a() << std::endl;
-  this->dataPtr->newEmissive.R() = _msg.r();
-  this->dataPtr->newEmissive.G() = _msg.g();
-  this->dataPtr->newEmissive.B() = _msg.b();
-  this->dataPtr->newEmissive.A() = _msg.a();
+  // In the update loop, you can for example get the name of the world and set
+  // it as a property that can be read from the QML.
+  if (_info.paused)
+    return;
 }
 
-IGNITION_ADD_PLUGIN(MaterialController,
-                    ignition::gazebo::System,
-                    MaterialController::ISystemConfigure,
-                    MaterialController::ISystemPreUpdate)
+/////////////////////////////////////////////////
+QString MaterialController::CustomProperty() const
+{
+  return this->customProperty;
+}
 
-IGNITION_ADD_PLUGIN_ALIAS(MaterialController,
-                          "ignition::gazebo::systems::MaterialController")
+/////////////////////////////////////////////////
+void MaterialController::SetCustomProperty(const QString &_customProperty)
+{
+  this->customProperty = _customProperty;
+  this->CustomPropertyChanged();
+}
+
+
+/////////////////////////////////////////////////
+/// \brief Function used to split a string by a delimiter character.
+/// \param[in] str std::string to be splited.
+/// \param[in] delim char to delimiter the strings separation.
+/// \return Returns a Vector of strings. std::vector<std::string>
+std::vector<std::string> MaterialController::SplitMsg(std::string const &str, const char delim)
+{
+  size_t start;
+  size_t end = 0;
+  std::vector<std::string> out;
+
+  while ((start = str.find_first_not_of(delim, end)) != std::string::npos)
+  {
+    end = str.find(delim, start);
+    out.push_back(str.substr(start, end - start));
+  }
+  return out;
+}
+// Register this plugin
+IGNITION_ADD_PLUGIN(MaterialController,
+                    ignition::gui::Plugin)
+
--- material_controller/MaterialController.hh
+++ material_controller/MaterialController.hh
@@ -1,49 +1,106 @@
 #ifndef MATERIALCONTROLLER_HH_
 #define MATERIALCONTROLLER_HH_
 
-#include <ignition/gazebo/System.hh>
-#include <memory>
+#include <ignition/gazebo/gui/GuiSystem.hh>
+#include <ignition/rendering/Scene.hh>
+#include <ignition/transport/Node.hh>
+#include "ignition/math/Color.hh"
 
-namespace ignition
+/// \brief Example of a GUI plugin that has access to entities and components.
+class MaterialController : public ignition::gazebo::GuiSystem
 {
-  namespace gazebo
-  {
-    // Inline bracket to help doxygen filtering.
-    inline namespace IGNITION_GAZEBO_VERSION_NAMESPACE {
-      namespace systems
-      {
-        // Forward declaration
-        class MaterialControllerPrivate;
-      
-        /// \brief This system applies a force to the first axis of a specified joint.
-        class MaterialController
-            : public System,
-              public ISystemConfigure,
-              public ISystemPreUpdate
-        {
-          /// \brief Constructor
-          public: MaterialController();
-      
-          /// \brief Destructor
-          public: ~MaterialController() override = default;
-      
-          // Documentation inherited
-          public: void Configure(const Entity &_entity,
-                                 const std::shared_ptr<const sdf::Element> &_sdf,
-                                 EntityComponentManager &_ecm,
-                                 EventManager &_eventMgr) override;
-      
-          // Documentation inherited
-          public: void PreUpdate(
-                      const ignition::gazebo::UpdateInfo &_info,
-                      ignition::gazebo::EntityComponentManager &_ecm) override;
-      
-          /// \brief Private data pointer
-          private: std::unique_ptr<MaterialControllerPrivate> dataPtr;
-        };
-      }
-    }
-  }
-}
+  Q_OBJECT
+
+    /// \brief Custom property. Use this to create properties that can be read
+    /// from the QML file. See the declarations below.
+    Q_PROPERTY(
+      QString customProperty
+      READ CustomProperty
+      WRITE SetCustomProperty
+      NOTIFY CustomPropertyChanged
+    )
+
+  /// \brief Constructor
+  public: MaterialController();
+
+  /// \brief Destructor
+  public: ~MaterialController() override;
+
+  /// \brief `ignition::gui::Plugin`s can overload this function to
+  /// receive custom configuration from an XML file. Here, it comes from the
+  /// SDF.
+  ///
+  /// <gui>
+  ///   <plugin ...> <!-- this is the plugin element -->
+  ///     ...
+  ///   </plugin>
+  /// </gui>
+  ///
+  /// \param[in] _pluginElem SDF <plugin> element. Will be null if the plugin
+  /// is loaded without any XML configuration.
+  public: void LoadConfig(const tinyxml2::XMLElement *_pluginElem) override;
+
+  /// \brief GUI systems can overload this function to receive updated simulation
+  /// state. This is called whenever the server sends state updates to the GUI.
+  /// \param[in] _info Simulation information such as time.
+  /// \param[in] _ecm Entity component manager, which can be used to get the
+  /// latest information about entities and components.
+  public: void Update(const ignition::gazebo::UpdateInfo &_info,
+      ignition::gazebo::EntityComponentManager &_ecm) override;
+
+  /// \brief Get the custom property as a string.
+  /// \return Custom property
+  public: Q_INVOKABLE QString CustomProperty() const;
+
+  /// \brief Set the custom property from a string.
+  /// \param[in] _customProperty Custom property
+  public: Q_INVOKABLE void SetCustomProperty(const QString &_customProperty);
+
+  /// \brief Notify that custom property has changed
+  signals: void CustomPropertyChanged();
+
+  /// \brief Callback when user clicks button.
+  public slots: void RandomColor();
+
+  /// \brief Callback for all installed event filters.
+  /// \param[in] _obj Object that received the event
+  /// \param[in] _event Event
+  private: bool eventFilter(QObject *_obj, QEvent *_event) override;
+
+  /// \brief All rendering operations must happen within this call
+  private: void PerformRenderingOperations();
+
+  /// \brief Encapsulates the logic to find the rendering scene through the
+  /// render engine singleton.
+  private: void FindScene();
+
+  /// \brief Callback for joint force subscription
+  /// \param[in] _msg Joint force message
+  public: void OnChangeCollor(const ignition::msgs::StringMsg &_msg);
+
+  /////////////////////////////////////////////////
+  /// \brief Function used to split a string by a delimiter character.
+  /// \param[in] str std::string to be splited.
+  /// \param[in] delim char to delimiter the strings separation.
+  /// \return Returns a Vector of strings. std::vector<std::string>
+  public: std::vector<std::string> SplitMsg(std::string const &str, const char delim);
+
+  /// \brief Marks when a new change has been requested.
+  private: bool dirty{false};
+
+  /// \brief Pointer to the rendering scene.
+  private: ignition::rendering::ScenePtr scene{nullptr};
+
+  /// \brief Ignition communication node.
+  public: ignition::transport::Node node;
+
+  /// \brief color to apply
+  public: ignition::math::Color newEmissive;
+
+  /// \brief Custom property
+  private: QString customProperty;
+
+  public: std::string linkName;
+};
 
 #endif
